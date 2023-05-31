98f8c7d5d2655aea564e50c19c6e859146dc6ceb
G.azzu <gneiazmah@gmail.com>
2023-06-01 02:14:15 +1000

    Initial commit

---
+++ app/src/main/java/com/example/sit708_my_application_01/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sit708_my_application_01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cv_tmp;
+    CardView cv_weight;
+    CardView cv_length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cv_tmp = findViewById(R.id.cv_tmp);
+        cv_weight = findViewById(R.id.cv_weight);
+        cv_length = findViewById(R.id.cv_length);
+
+        cv_tmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, temperature.class));
+            }
+        });
+        cv_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, weight.class));
+            }
+        });
+        cv_length.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, length.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sit708_my_application_01/length.java
@@ -0,0 +1,14 @@
+package com.example.sit708_my_application_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class length extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_length);
+    }
+}
